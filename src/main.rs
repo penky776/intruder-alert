07cb55dc3ab0e639cef8700b7a80a30981f21a97
penky <79907738+penky776@users.noreply.github.com>
2023-04-08 12:28:08 +0500

    made some changes

--- src/main.rs
+++ src/main.rs
@@ -9,53 +9,53 @@ use pnet::{
         },
         ip::IpNextHeaderProtocols,
     },
-    transport::{
-        transport_channel, TransportChannelType::Layer4, TransportProtocol::Ipv4, TransportReceiver,
-    },
+    transport::{transport_channel, TransportChannelType::Layer4, TransportProtocol::Ipv4},
 };
-use rayon::prelude::*;
+use rayon;
 use std::{
     io::Error,
     net::{IpAddr, Ipv4Addr},
-    thread,
 };
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let protocol = Layer4(Ipv4(IpNextHeaderProtocols::Icmp));
 
-    (1..=255).into_par_iter().for_each(|x| {
-        let (mut tx, rx) = match transport_channel(4096, protocol) {
-            Ok((tx, rx)) => (tx, rx),
-            Err(e) => panic!(
-                "An error occurred when creating the transport channel: {}",
-                e
-            ),
-        };
-        let mut buffer = [0u8; 16];
-        let icmp_req_packet = create_packet(&mut buffer);
-        let ip_address = IpAddr::V4(Ipv4Addr::new(192, 168, 100, x));
-        thread::scope(|s| {
-            s.spawn(|| {
-                match tx.send_to(icmp_req_packet, ip_address) {
-                    Ok(n) => n,
-                    Err(e) => panic!("failed to send packet: {}", e),
-                };
-            });
-        });
+    let pool = rayon::ThreadPoolBuilder::new()
+        .num_threads(255)
+        .build()
+        .unwrap();
 
-        match read_from_res(rx) {
-            Ok(s) => println!("{}: {}", ip_address, s),
-            Err(e) => println!("{}: {}", ip_address, e.to_string()),
-        };
-    });
+    for i in 0..=255 {
+        pool.install(|| {
+            let (mut tx, rx) = match transport_channel(4096, protocol) {
+                Ok((tx, rx)) => (tx, rx),
+                Err(e) => panic!(
+                    "An error occurred when creating the transport channel: {}",
+                    e
+                ),
+            };
+            let mut buffer = [0u8; 16];
+            let icmp_req_packet = create_packet(&mut buffer);
+            let ip_address = IpAddr::V4(Ipv4Addr::new(192, 168, 100, i));
+            match tx.send_to(icmp_req_packet, ip_address) {
+                Ok(n) => n,
+                Err(e) => panic!("failed to send packet: {}", e),
+            };
+
+            let sd = rx.socket.fd;
+
+            match read_from_res(sd) {
+                Ok(s) => println!("{}: {}", ip_address, s),
+                Err(e) => println!("{}: {}", ip_address, e.to_string()),
+            };
+        });
+    }
 
     Ok(())
 }
 
-fn read_from_res(rx: TransportReceiver) -> Result<String, Error> {
-    let sd = rx.socket.fd;
-
+fn read_from_res(sd: i32) -> Result<String, Error> {
     let mut res = [0u8; 200]; // the response packet
     let buf_point: *mut c_void = res.as_mut_ptr() as *mut c_void;
 
@@ -69,7 +69,7 @@ fn read_from_res(rx: TransportReceiver) -> Result<String, Error> {
 
     if let Some(v) = res_protocol.unwrap().unwrap().icmpv4() {
         match v.icmp_type {
-            Icmpv4Type::EchoReply(_) => Ok(String::from("Host available")),
+            Icmpv4Type::EchoReply(_) => Ok(String::from("Echo Reply received")),
             Icmpv4Type::DestinationUnreachable(_) => Ok(String::from("Destination unreachable")),
             _ => Ok(String::from("Unexpected")),
         }
