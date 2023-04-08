bf0760a293248ab202182fc388ed7a22bc6df1a7
penky <79907738+penky776@users.noreply.github.com>
2023-04-08 06:25:26 +0500

    network scanning

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,19 @@ use etherparse::{Icmpv4Type, PacketHeaders, ReadError, TransportHeader};
 use libc::{self, c_void, read};
 use pnet::{
     packet::{
-        icmp::{echo_request, IcmpTypes},
+        icmp::{
+            echo_request::{self, MutableEchoRequestPacket},
+            IcmpTypes,
+        },
         ip::IpNextHeaderProtocols,
     },
-    transport::{transport_channel, TransportChannelType::Layer4, TransportProtocol::Ipv4},
+    transport::{
+        transport_channel, TransportChannelType::Layer4, TransportProtocol::Ipv4, TransportReceiver,
+    },
 };
+use rayon::prelude::*;
 use std::{
+    io::Error,
     net::{IpAddr, Ipv4Addr},
     thread,
 };
@@ -17,35 +24,36 @@ use std::{
 async fn main() -> std::io::Result<()> {
     let protocol = Layer4(Ipv4(IpNextHeaderProtocols::Icmp));
 
-    let mut buffer = [0u8; 16];
-
-    let mut icmp_req_packet = echo_request::MutableEchoRequestPacket::new(&mut buffer).unwrap();
-    icmp_req_packet.set_icmp_type(IcmpTypes::EchoRequest);
-    icmp_req_packet.set_sequence_number(0);
-    let payload = [1, 2, 3, 4, 5, 6, 7, 8];
-    icmp_req_packet.set_payload(&payload);
-    icmp_req_packet.set_checksum(59371);
-
-    let (mut tx, rx) = match transport_channel(4096, protocol) {
-        Ok((tx, rx)) => (tx, rx),
-        Err(e) => panic!(
-            "An error occurred when creating the transport channel: {}",
-            e
-        ),
-    };
-
-    thread::scope(|s| {
-        s.spawn(|| {
-            match tx.send_to(
-                icmp_req_packet,
-                IpAddr::V4(Ipv4Addr::new(192, 168, 100, 14)),
-            ) {
-                Ok(n) => n,
-                Err(e) => panic!("failed to send packet: {}", e),
-            }
+    (1..=255).into_par_iter().for_each(|x| {
+        let (mut tx, rx) = match transport_channel(4096, protocol) {
+            Ok((tx, rx)) => (tx, rx),
+            Err(e) => panic!(
+                "An error occurred when creating the transport channel: {}",
+                e
+            ),
+        };
+        let mut buffer = [0u8; 16];
+        let icmp_req_packet = create_packet(&mut buffer);
+        let ip_address = IpAddr::V4(Ipv4Addr::new(192, 168, 100, x));
+        thread::scope(|s| {
+            s.spawn(|| {
+                match tx.send_to(icmp_req_packet, ip_address) {
+                    Ok(n) => n,
+                    Err(e) => panic!("failed to send packet: {}", e),
+                };
+            });
         });
+
+        match read_from_res(rx) {
+            Ok(s) => println!("{}: {}", ip_address, s),
+            Err(e) => println!("{}: {}", ip_address, e.to_string()),
+        };
     });
 
+    Ok(())
+}
+
+fn read_from_res(rx: TransportReceiver) -> Result<String, Error> {
     let sd = rx.socket.fd;
 
     let mut res = [0u8; 200]; // the response packet
@@ -61,11 +69,21 @@ async fn main() -> std::io::Result<()> {
 
     if let Some(v) = res_protocol.unwrap().unwrap().icmpv4() {
         match v.icmp_type {
-            Icmpv4Type::EchoReply(_) => println!("Host available"),
-            Icmpv4Type::DestinationUnreachable(_) => println!("Destination Unreachable"),
-            _ => println!("Unexpected"),
+            Icmpv4Type::EchoReply(_) => Ok(String::from("Host available")),
+            Icmpv4Type::DestinationUnreachable(_) => Ok(String::from("Destination unreachable")),
+            _ => Ok(String::from("Unexpected")),
         }
+    } else {
+        return Ok(String::from("Unexpected"));
     }
+}
 
-    Ok(())
+fn create_packet(buffer: &mut [u8; 16]) -> MutableEchoRequestPacket {
+    let mut icmp_req_packet = echo_request::MutableEchoRequestPacket::new(buffer).unwrap();
+    icmp_req_packet.set_icmp_type(IcmpTypes::EchoRequest);
+    icmp_req_packet.set_sequence_number(0);
+    let payload = [1, 2, 3, 4, 5, 6, 7, 8];
+    icmp_req_packet.set_payload(&payload);
+    icmp_req_packet.set_checksum(59371);
+    icmp_req_packet
 }
